11f7aab5fc0bf792e00881f5fbbf3c0bd75b6318
Nea-dev04 <webmoticnea@gmail.com>
2026-02-23 15:20:53 +0300

    Refactor burger menu and dark mode functionality

--- js/main.js
+++ js/main.js
@@ -1,32 +1,22 @@
-// burger menu
-
+// BURGER MENU
 const burger = document.getElementById("burger");
 const nav = document.getElementById("nav");
 
 burger.addEventListener("click", () => {
-
-nav.classList.toggle("active");
-
+  nav.classList.toggle("active");
 });
 
-
-// dark mode
-
+// DARK MODE
 const toggle = document.getElementById("darkToggle");
 
-if(localStorage.getItem("darkMode") === "true"){
-
-document.body.classList.add("dark");
-
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark");
 }
 
 toggle.addEventListener("click", () => {
-
-document.body.classList.toggle("dark");
-
-localStorage.setItem(
-"darkMode",
-document.body.classList.contains("dark")
-);
-
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "darkMode",
+    document.body.classList.contains("dark")
+  );
 });
